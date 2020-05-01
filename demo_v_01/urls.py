79b3472c7fbe2df5f2336d728279f3f6d973c689
behi <behradazimi1@gmail.com>
2020-05-01 19:41:31 +0430

    still trying, forth time

--- demo_v_01/urls.py
+++ demo_v_01/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
-    path('', include('login_page.urls')),
+    #path('', include('login_page.urls')),
 ]
--- login_page/views.py
+++ login_page/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TempaleView
+#from django.views.generic import TempaleView
 
 # Create your views here.
-class HomePageView(TemplateView):
-    tempalte_name="home.html"
-    
+#class HomePageView(TemplateView):
+#    tempalte_name="home.html"
