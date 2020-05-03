270df37f18f350c993776b4d5f1140912fafd7cc
behi <behradazimi1@gmail.com>
2020-05-03 10:54:35 +0430

    on trial 8 i decided to create another prokect and try a new way on that project

--- demo_v_01/urls.py
+++ demo_v_01/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
@@ -30,3 +31,4 @@ urlpatterns += [
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- login_page/views.py
+++ login_page/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 #class HomePageView(TemplateView):
 #    tempalte_name="home.html"
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
