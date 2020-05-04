0d3f57872e67cab16a9e0b82b05394174914aa9f
behi <behradazimi1@gmail.com>
2020-05-04 19:04:53 +0430

    app deleted and login system works perfectly

--- login_page/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import views
-from django.urls import path
-from .views import *
-from . import views
-app_name = 'login_page'
-
-urlpatterns = [
-   path('login/', views.loginView.as_view(), name='login'),
-]
--- login_page/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-#from django.views.generic import TempaleView
-
-# Create your views here.
-#class HomePageView(TemplateView):
-#    tempalte_name="home.html"
