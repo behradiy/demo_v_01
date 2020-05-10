6fe9f319a9b8fc36c3180ae5a615b0bbffe79b12
behi <behradazimi1@gmail.com>
2020-05-10 23:49:01 +0430

    second try on merging

--- demo_v_01/settings.py
+++ demo_v_01/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 
 ]
 
@@ -138,9 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-=======
+
 
 # adding crispy forms for sign up
 CRISPY_TEMPLATE_PACK = "bootstrap4"
->>>>>>> login
