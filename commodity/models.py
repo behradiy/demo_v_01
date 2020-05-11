3910615a1e91768bab5d07c8b1731c72205dc508
javadmusavi <javadmusavi.666@gmail.com>
2020-05-12 01:03:25 +0430

    Commodity and category first models and commits

--- commodity/models.py
+++ commodity/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Commoditie (models.Model):
+    Category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    Name = models.CharField(max_length=50)
+    Marketer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    Price = models.BigIntegerField()
+    SelesNumber = models.IntegerField()
+    Popularity=models.IntegerField()
+    Views=models.IntegerField()
+    AmountinStore = models.IntegerField()
+    About = models.CharField(max_length=5000)
+    image = models.ImageField()
+
+class Category (models.Model):
+    
+    Name=models.CharField(max_length=50)
+    selesNumber=models.IntegerField()
+    Popularity = models.IntegerField()
+    views = models.IntegerField()
+    Image = models.ImageField()
+
+
+
+
+
+
 
 # Create your models here.
--- commodity/views.py
+++ commodity/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpRequest
+def main (request):
+    
+
+
 
 # Create your views here.
