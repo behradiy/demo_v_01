6a0a576f3cea28814562ce3ba4efeb1d6d4185d0
javadmusavi <javadmusavi.666@gmail.com>
2020-05-12 01:19:40 +0430

    first view method and template of the Commodity

--- commodity/views.py
+++ commodity/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpRequest
+from django.http import HttpRequest,HttpResponse
+from django.template import loader
 def mainpage(request):
-
+    cms = Commoditie.objects.all()
+    context = {'commodity': cms, }
+    t = loader.get_template('commodity/CommodityMain.html')
+    t.render(context)
+    return HttpResponse(t)
 
 
 
